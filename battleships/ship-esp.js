806d5d5b23ad99dcaf00e890da99d87fbf6d5ff5
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2022-10-06 20:51:14 +0100

    Update ship-esp.js

--- battleships/ship-esp.js
+++ battleships/ship-esp.js
@@ -1,1 +1,21 @@
-const board=document.getElementById("opponent-board");let ships=board.querySelectorAll("[ship]");const showShips=()=>{ships.forEach((e=>{if("empty"!=e.getAttribute("ship")){board.querySelectorAll(`[ship="${e.getAttribute("ship")}"]`).forEach((e=>{e.style.border="2px solid red"}))}}))};socket.on("takeTurnResponse",(e=>{"shootCoordinate"==e.turnType&&ships.forEach((e=>{"empty"!=e.getAttribute("ship")&&board.querySelectorAll(`[ship="${e.getAttribute("ship")}"]`).forEach((e=>{e.style.border="2px solid red"}))}))}));showShips();
+const board = document.getElementById("opponent-board");
+let ships = board.querySelectorAll("[ship]");
+
+const showShips = () => {
+    ships.forEach(ship => {
+        if (ship.getAttribute("ship") != "empty") {
+            let shipElements = board.querySelectorAll(`[ship="${ship.getAttribute("ship")}"]`);
+            shipElements.forEach(shipElement => {
+                shipElement.style.border = "2px solid red";
+            });
+        }
+    });
+}
+
+socket.on("takeTurnResponse", (e) => {
+    if (e.turnType == "shootCoordinate") {
+        showShips();
+    }
+})
+
+showShips();
